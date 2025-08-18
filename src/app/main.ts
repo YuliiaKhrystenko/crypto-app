b276ae61af0b8deeb5918bac8d7656f003a6cc8e
Yuliia Khrystenkoe <yuliia.khrystenko@right.link>
2025-08-18 21:20:02 +0300

    feat: DCA Pinia store with IndexedDB + Zod validation

--- src/app/main.ts
+++ src/app/main.ts
@@ -7,10 +7,13 @@ import App from '../App.vue'
 import { router } from '../router'
 
 import { createPinia } from 'pinia'
+import { useDcaStore } from '@stores/dca'
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(VueQueryPlugin, { queryClient: new QueryClient() })
 app.use(router)
+
+await useDcaStore().init()
 app.mount('#app')
